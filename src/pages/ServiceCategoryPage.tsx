8ed3a2c19997415ca350d31dbb602b36e73b9759
Shevchenko433 <shevchenkoav433@gmail.com>
2024-11-18 14:21:02 +0100

    Add Home.tsx and update App.tsx, UserPage.tsx, and ServicePage.tsx

--- src/pages/ServiceCategoryPage.tsx
+++ src/pages/ServiceCategoryPage.tsx
@@ -1,15 +1,32 @@
+// src/pages/ServiceCategoryPage.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ServiceCategoryPage: React.FC = () => {
   return (
     <div>
-      <h1 className='text-xxl text-center mb-8 mt-8'>Service Categories</h1>
-      <div className="grid grid-cols-4 gap-4 text-center ">
-        <div className="col-start-2 bg-stone-50 p-8 rounded shadow-md hover:shadow-lg  cursor-pointer">Cats</div>
-        <div className=" bg-stone-50 p-8 rounded shadow-md hover:shadow-lg cursor-pointer">Dogs</div>
-        <div className="col-start-2 bg-stone-50 p-8 rounded shadow-md hover:shadow-lg cursor-pointer">Birds</div>
-        <div className=" bg-stone-50 p-8 rounded shadow-md hover:shadow-lg cursor-pointer">Rodents</div>
-        
+      <h1 className="text-xxl text-center mb-8 mt-8">Service Categories</h1>
+      <div className="grid grid-cols-4 gap-4 text-center">
+        <Link to="/services/cats" className="col-start-2">
+          <div className="bg-stone-50 p-8 rounded shadow-md hover:shadow-lg cursor-pointer">
+            Cats
+          </div>
+        </Link>
+        <Link to="/services/dogs">
+          <div className="bg-stone-50 p-8 rounded shadow-md hover:shadow-lg cursor-pointer">
+            Dogs
+          </div>
+        </Link>
+        <Link to="/services/birds" className="col-start-2">
+          <div className="bg-stone-50 p-8 rounded shadow-md hover:shadow-lg cursor-pointer">
+            Birds
+          </div>
+        </Link>
+        <Link to="/services/rodents">
+          <div className="bg-stone-50 p-8 rounded shadow-md hover:shadow-lg cursor-pointer">
+            Rodents
+          </div>
+        </Link>
       </div>
     </div>
   );
