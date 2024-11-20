bfacf901cb6399c0aff29323a581d210d127d2b8
Shevchenko433 <shevchenkoav433@gmail.com>
2024-11-20 12:09:38 +0100

    Добавлены переменные API и настроены конфигурации для apiService

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// src/config.ts
+
+export const API_USERS = 'http://localhost:8080/api/users';
+export const API_PETS = 'http://localhost:8080/api/pets';
+export const API_SERVICES = 'http://localhost:8080/api/services';
+export const API_CATIGORIES = 'http://localhost:8080/api/categories';
+export const API_CONTACTS = 'http://localhost:8080/api/contacts';
